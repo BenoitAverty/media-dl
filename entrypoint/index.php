a965ea2e15fcc027dd729e51b17154d9bd164af0
Benoit Averty <benoit.averty@gmail.com>
2017-06-26 23:40:56 +0200

    feat(entrypoint): Make a basic homepage and redirect paths without trailing /

---
+++ entrypoint/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>ab0 media-dl</title>
+  </head>
+  <body>
+    <ul>
+      <li><a href="sonarr/" title="sonarr">Sonarr (TV series)</a></li>
+      <li><a href="radarr/" title="radarr">Radarr (Movies)</a></li>
+      <li><a href="nzbget/" title="nzbget">Nzbget (Usenet downloader)</a></li>
+      <li><a href="torrent/" title="rutorrent">Rutorrent (torrent web client)</a></li>
+    </ul>
+  </body>
+</html>
